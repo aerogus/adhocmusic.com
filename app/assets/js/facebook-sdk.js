ed298f2eb1924f58c9348ed56f002645b2d53a98
Guillaume Seznec <guillaume.seznec@gmail.com>
2016-09-11 16:59:05 +0200

    update  fb sdk js en 2.7

--- app/assets/js/facebook-sdk.js
+++ app/assets/js/facebook-sdk.js
@@ -1,7 +1,15 @@
+window.fbAsyncInit = function() {
+  FB.init({
+    appId      : '50959607741',
+    xfbml      : true,
+    version    : 'v2.7'
+  });
+};
+
 (function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/fr_FR/sdk.js#xfbml=1&version=v2.5&appId=50959607741";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
+   var js, fjs = d.getElementsByTagName(s)[0];
+   if (d.getElementById(id)) return;
+   js = d.createElement(s); js.id = id;
+   js.src = "//connect.facebook.net/fr_FR/sdk.js";
+   fjs.parentNode.insertBefore(js, fjs);
+ }(document, 'script', 'facebook-jssdk'));
