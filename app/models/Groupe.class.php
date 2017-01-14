72f4ed120cb26560f81e525b559d0793f44e494b
Guillaume Seznec <guillaume.seznec@gmail.com>
2017-01-14 17:36:10 +0100

    ajout url membre

--- app/models/Groupe.class.php
+++ app/models/Groupe.class.php
@@ -1236,6 +1236,7 @@ class Groupe extends ObjectModel
         foreach($res as $_res) {
             $res[$cpt]['id'] = intval($_res['id']);
             $res[$cpt]['nom_type_musicien'] = Membre::getTypeMusicienName($_res['id_type_musicien']);
+            $res[$cpt]['url'] = Membre::getUrlById($_res['id']);
             $cpt++;
         }
         return $res;
