c11a616b467b1a59f0757a48d10df39aa25dcf4b
Guillaume Seznec <guillaume.seznec@gmail.com>
2018-02-15 12:08:33 +0100

    gestion abonnés forums privés

---
+++ app/scripts/forum-prive-manager.php
@@ -0,0 +1,29 @@
+#!/usr/bin/php
+<?php
+
+require_once dirname(__FILE__) . '/../config.php';
+
+// id_contact à ajouter
+$to_add = [
+2819877
+];
+
+// ajouter des personnes aux forums privés
+foreach (['a', 'b', 'e', 's', 't'] as $id_forum) {
+  foreach ($to_add as $id_contact) {
+    ForumPrive::addSubscriberToForum($id_contact, $id_forum);
+  }
+}
+
+// retirer des personnes aux forums privés
+//ForumPrive::delSubscriberToForum(3, 's');
+/*
+// id_contact à retirer
+$to_del = [
+];
+
+foreach ($to_del as $id_contact) {
+  $r = ForumPrive::delAllSubscriptions($id_contact);
+  var_dump($r);
+}
+*/
