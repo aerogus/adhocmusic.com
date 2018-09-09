81937f98c0a62b776e286dec18be1fd90303243b
Guillaume Seznec <guillaume@seznec.fr>
2018-09-09 16:21:23 +0200

    suite docker

---
+++ initHosts.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# Ajout des hostnames des conteneurs docker
+#
+
+rules="127.0.0.1 adhocmusic.test www.adhocmusic.test static.adhocmusic.test php.adhocmusic.test mysql.adhocmusic.test pma.adhocmusic.test"
+hosts="/etc/hosts"
+
+if ! grep -q "$rules" $hosts; then
+  echo "Règles de hosts à ajouter dans $hosts (nécessite root)"
+  echo "$rules"
+  echo "$rules" >> $hosts
+else
+  echo "Règles de hosts OK dans $hosts"
+fi
