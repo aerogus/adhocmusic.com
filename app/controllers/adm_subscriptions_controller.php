8152d879f84307d30775b5ae7d2b91288fc4138f
Guillaume Seznec <guillaume@seznec.fr>
2019-09-15 21:43:18 +0200

    début outils de gestion cotisation

---
+++ app/controllers/adm_subscriptions_controller.php
@@ -0,0 +1,74 @@
+<?php
+
+/**
+ *
+ */
+final class Controller
+{
+    /**
+     * @return string
+     */
+    static function index(): string
+    {
+        Tools::auth(Membre::TYPE_INTERNE);
+
+        $smarty = new AdHocSmarty();
+
+        $trail = Trail::getInstance();
+        $trail->addStep("Privé", "/adm/");
+        $trail->addStep("Cotisations");
+
+        return $smarty->fetch('adm/subscriptions/index.tpl');
+    }
+
+    /**
+     * @return string
+     */
+    static function create(): string
+    {
+        Tools::auth(Membre::TYPE_INTERNE);
+
+        $smarty = new AdHocSmarty();
+
+        $trail = Trail::getInstance();
+        $trail->addStep("Privé", "/adm/");
+        $trail->addStep("Cotisations", "/adm/subscriptions");
+        $trail->addStep("Ajout");
+
+        return $smarty->fetch('adm/subscriptions/create.tpl');
+    }
+
+    /**
+     * @return string
+     */
+    static function edit(): string
+    {
+        Tools::auth(Membre::TYPE_INTERNE);
+
+        $smarty = new AdHocSmarty();
+
+        $trail = Trail::getInstance();
+        $trail->addStep("Privé", "/adm/");
+        $trail->addStep("Cotisations", "/adm/subscriptions");
+        $trail->addStep("Édition");
+
+        return $smarty->fetch('adm/subscriptions/edit.tpl');
+    }
+
+    /**
+     * @return string
+     */
+    static function delete(): string
+    {
+        Tools::auth(Membre::TYPE_INTERNE);
+
+        $smarty = new AdHocSmarty();
+
+        $trail = Trail::getInstance();
+        $trail->addStep("Privé", "/adm/");
+        $trail->addStep("Cotisations", "/adm/subscriptions");
+        $trail->addStep("Suppression");
+
+        return $smarty->fetch('adm/subscriptions/delete.tpl');
+    }
+}
---
+++ app/views/adm/subscriptions/index.tpl
@@ -0,0 +1,7 @@
+{include file="common/header.tpl"}
+
+{include file="common/boxstart.tpl" boxtitle="Cotisations"}
+
+{include file="common/boxend.tpl"}
+
+{include file="common/footer.tpl"}
