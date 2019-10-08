746392ebc5af32375acccf316f373fca72994db3
Guillaume Seznec <guillaume@seznec.fr>
2019-10-08 09:05:11 +0200

    début new style

---
+++ app/cli/dataset/styles.php
@@ -0,0 +1,47 @@
+#!/usr/bin/env php
+<?php declare(strict_types=1);
+
+/**
+ * Initialise des styles
+ */
+
+require_once __DIR__ . '/../../bootstrap.php';
+
+$styles = ["Rock",
+       "Reggae",
+     "Techno",
+      "Métal",
+      "World",
+  "Chanson",
+ "Hip Hop",
+      "R'n'B",
+        "Jazz",
+         "Funk",
+   "Disco",
+     "Classique",
+    "Pop",
+    "Progressif",
+       "Punk",
+        "Ska",
+       "Trip Hop",
+        "Fusion",
+       "Dub",
+        "Heavy",
+     "Drum 'n Bass",
+       "Celtique",
+       "House",
+     "Electro",
+ "Soul",
+     "Blues",
+        "Festif",
+ "Raï",
+     "Folk",
+        "Latino",
+     "Country",
+];
+
+foreach ($styles as $s) {
+    Style::init()->setName($s)->save();
+}
+
+var_dump(Style::findAll());
--- app/models/Style.class.php
+++ app/models/Style.class.php
@@ -1,15 +1,105 @@
 <?php declare(strict_types=1);
 
 /**
- * Classe Style
- *
  * Classe de gestion des styles musicaux
  *
  * @package AdHoc
  * @author  Guillaume Seznec <guillaume@seznec.fr>
  */
-class Style
+class Style extends ObjectModel
 {
+    /**
+     * @var object
+     */
+    protected static $_instance = null;
+
+    /**
+     * @var string
+     */
+    protected static $_pk = 'id_style';
+    /**
+     * @var string
+     */
+    protected static $_table = 'adhoc_style';
+
+    /**
+     * @var int
+     */
+    protected $_id_style = 0;
+
+    /**
+     * @var string
+     */
+    protected $_name = '';
+
+    /**
+     * Liste des attributs de l'objet
+     * on précise si en base c'est de type :
+     * - numérique/integer/float/bool (= int)
+     * - datetime/text (= str)
+     * ceci est utile pour la formation de la requête
+     *
+     * @var array
+     */
+    protected static $_all_fields = [
+        'name' => 'str',
+    ];
+
+    /**
+     * Tableau des attributs modifiés depuis la dernière sauvegarde.
+     *
+     * Pour chaque attribut modifié, on a un élément de la forme 'attribut => true'.
+     *
+     * @var array
+     */
+    protected $_modified_fields = [];
+
+    /* début getters */
+
+    /**
+     * @return string
+     */
+    static function getName(): string
+    {
+        return $this->_name;
+    }
+
+    /* fin getters */
+
+    /* début setters */
+
+    /**
+     * @param string $val val
+     *
+     * @return object
+     */
+    function setName(string $val): object
+    {
+        if ($this->_name !== $val) {
+            $this->_name = $val;
+            $this->_modified_fields['name'] = true;
+        }
+
+        return $this;
+    }
+
+    /* fin setters */
+
+    /**
+     * Retourne les infos sur un style
+     *
+     * @return bool
+     * @throws Exception
+     */
+    function _loadFromDb(): bool
+    {
+        if (!parent::_loadFromDb()) {
+            throw new Exception('Style introuvable');
+        }
+
+        return true;
+    }
+
     /**
      * Codes style
      */
@@ -93,18 +183,6 @@ class Style
     {
         asort(self::$_liste);
         return self::$_liste;
-    }
-
-    /**
-     * Retourne le libellé d'une clé de la liste
-     *
-     * @param int $cle clé
-     *
-     * @return string
-     */
-    static function getName(int $cle): string
-    {
-        return self::$_liste[$cle];
     }
 
     /**
