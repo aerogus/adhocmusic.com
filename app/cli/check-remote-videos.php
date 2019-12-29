e3fd5485f81b0ef95b85f0fb9b02ffda02e610c9
Guillaume Seznec <guillaume@seznec.fr>
2019-12-29 12:34:15 +0100

    fix

--- app/cli/check-remote-videos.php
+++ app/cli/check-remote-videos.php
@@ -6,11 +6,12 @@ require_once __DIR__ . '/../bootstrap.php';
 // vérifie si les vidéos distances sont tjrs valides / publiques
 // Facebook renvoie un 302 vers unsupported browser...
 
+define('UTEMPO', 250000); // 250ms
+
 $videos = Video::findAll();
 
 foreach ($videos as $video) {
     $hdrs = get_headers($video->getEmbedUrl());
     echo "Video " . $video->getId() . ": " . $hdrs[0] . " " . $video->getEmbedUrl() . "\n";
-    usleep(250000);
-}
+    usleep(UTEMPO);
 }
