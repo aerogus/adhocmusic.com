d1b3842bd8c10ffc9e065fb4a1a79eee8341872e
Guillaume Seznec <guillaume@seznec.fr>
2020-06-21 12:34:01 +0200

    leaflet

--- public/js/lieux/show.js
+++ public/js/lieux/show.js
@@ -1,8 +1,11 @@
-/*globals jQuery,Masonry,imagesLoaded,baguetteBox,google,asv */
+/*globals jQuery,Masonry,imagesLoaded,baguetteBox,L,asv */
 
 jQuery(document).ready(function () {
 
   'use strict';
+
+  var map = L.map('map_canvas').setView([asv.lat, asv.lng], 13);
+  var marker = L.marker([asv.lat, asv.lng]).addTo(map);
 
   if (document.getElementsByClassName('gallery').length > 0) {
 
@@ -23,25 +26,3 @@ jQuery(document).ready(function () {
   }
 
 });
-
-function adhocLieuInitMap()
-{
-  var center = new google.maps.LatLng({lat: asv.lieu.lat, lng: asv.lieu.lng});
-  var map = new google.maps.Map(document.getElementById("map_canvas"), {
-    zoom: 15,
-    center: center,
-    mapTypeId: google.maps.MapTypeId.HYBRID,
-    disableDefaultUI: true
-  });
-  var image = new google.maps.MarkerImage('/img/pin/note.png',
-    new google.maps.Size(32, 32), // taille
-    new google.maps.Point(0,0),   // origine
-    new google.maps.Point(16, 16)  // ancre
-  );
-  var marker = new google.maps.Marker({
-    position: center,
-    map: map,
-    icon: image,
-    title: asv.lieu.name
-  });
-};
