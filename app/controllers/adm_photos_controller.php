f4d73c5ca3e2bd8372c819cfbc02630bc54e0ebb
Guillaume Seznec <guillaume@seznec.fr>
2022-03-13 16:10:49 +0100

    fix warning

--- app/controllers/adm_photos_controller.php
+++ app/controllers/adm_photos_controller.php
@@ -1,5 +1,7 @@
 <?php declare(strict_types=1);
 
+define('NB_PHOTOS_PER_PAGE', 80);
+
 final class Controller
 {
     /**
@@ -13,16 +15,26 @@ final class Controller
             ->addStep("Privé", "/adm")
             ->addStep("Photos");
 
+        $page = (int) Route::params('page');
+
         $smarty = new AdHocSmarty();
 
-        $smarty->assign(
-            'photos', Photo::find(
-                [
-                    'order_by' => 'id_photo',
-                    'sort' => 'ASC',
-                ]
-            )
+        $photos = Photo::find(
+            [
+                'order_by' => 'id_photo',
+                'sort' => 'ASC',
+                'start' => $page * NB_PHOTOS_PER_PAGE,
+                'limit' => NB_PHOTOS_PER_PAGE,
+            ]
         );
+        $nb_photos = count($photos);
+
+        $smarty->assign('photos', $photos);
+
+        // pagination
+        $smarty->assign('nb_items', $nb_photos);
+        $smarty->assign('nb_items_per_page', NB_PHOTOS_PER_PAGE);
+        $smarty->assign('page', $page);
 
         return $smarty->fetch('adm/photos/index.tpl');
     }
