0426c8265270052cd1bba75cbf4309c5f4a59bb6
Guillaume Seznec <guillaume@seznec.fr>
2024-03-13 21:45:35 +0100

    tabeaux

---
+++ tests_old/departement.php
@@ -0,0 +1,28 @@
+#!/usr/bin/env php
+<?php
+
+declare(strict_types=1);
+
+use Adhoc\Model\Reference\Departement;
+use Adhoc\Utils\LogNG;
+
+require_once __DIR__ . '/../app/bootstrap.php';
+
+$ds = Departement::find([
+    'order_by' => 'id_departement',
+    'sort' => 'ASC',
+]);
+
+$tbl = new \Console_Table();
+$tbl->setHeaders(
+    ['id_departement', 'name']
+);
+
+foreach ($ds as $d) {
+    $row = [];
+    $row[] = $d->getIdDepartement();
+    $row[] = $d->getName();
+    $tbl->addRow($row);
+}
+
+echo $tbl->getTable();
---
+++ tests_old/world-region.php
@@ -0,0 +1,30 @@
+#!/usr/bin/env php
+<?php
+
+declare(strict_types=1);
+
+use Adhoc\Model\Reference\WorldRegion;
+use Adhoc\Utils\LogNG;
+
+require_once __DIR__ . '/../app/bootstrap.php';
+
+$wrs = WorldRegion::find([
+    'order_by' => 'id_country',
+    'sort' => 'ASC',
+]);
+
+$tbl = new \Console_Table();
+$tbl->setHeaders(
+    ['id_country', 'id_region', 'world_country.name',  'world_region.name']
+);
+
+foreach ($wrs as $wr) {
+    $row = [];
+    $row[] = $wr->getIdCountry();
+    $row[] = $wr->getIdRegion();
+    $row[] = $wr->getCountry()->getName();
+    $row[] = $wr->getName();
+    $tbl->addRow($row);
+}
+
+echo $tbl->getTable();
