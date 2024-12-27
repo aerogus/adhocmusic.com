671940ad3ebd07a8fdb84d6bca714be40786d550
Guillaume Seznec <guillaume@seznec.fr>
2024-12-27 19:54:13 +0100

    move

--- src/Adhoc/Utils/AdHocTwigBootstrap.php
+++ src/Adhoc/Utils/AdHocTwigBootstrap.php
@@ -44,7 +44,7 @@ class AdHocTwigBootstrap
      */
     public function __construct()
     {
-        $this->loader = new FilesystemLoader(TWIG_TEMPLATES_PATH . '_bs');
+        $this->loader = new FilesystemLoader(TWIG_TEMPLATES_PATH);
         $this->env = new Environment($this->loader, [
             'cache' => TWIG_TEMPLATES_C_PATH,
             'auto_reload' => true, // true pour dev uniquement
