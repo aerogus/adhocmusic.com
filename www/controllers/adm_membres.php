732ddb332c9604158e395a5417c9b5ef7b78e167
Guillaume Seznec <guillaume@seznec.fr>
2026-03-08 17:30:32 +0100

    ménage

--- www/controllers/adm_membres.php
+++ www/controllers/adm_membres.php
@@ -18,41 +18,10 @@ final class Controller
     {
         Tools::auth(Membre::TYPE_INTERNE);
 
-        if (((string) Route::params('sort') === 'DESC')) {
-            $sort = 'DESC';
-            $sortinv = 'ASC';
-        } else {
-            $sort = 'ASC';
-            $sortinv = 'DESC';
-        }
-
-        if (!is_null(Route::params('order_by'))) {
-            $order_by = Route::params('order_by');
-        } else {
-            $order_by = 'id_contact';
-        }
-
-        $page = (int) Route::params('page');
-
         $pseudo = trim((string) Route::params('pseudo'));
         $email = trim((string) Route::params('email'));
         $last_name = trim((string) Route::params('last_name'));
         $first_name = trim((string) Route::params('first_name'));
-
-        $membres = Membre::find(
-            [
-                'pseudo'     => $pseudo,
-                'email'      => $email,
-                'last_name'  => $last_name,
-                'first_name' => $first_name,
-                'order_by'   => $order_by,
-                'sort'       => $sort,
-                'start'      => $page * self::MEMBERS_PER_PAGE,
-                'limit'      => self::MEMBERS_PER_PAGE,
-            ]
-        );
-
-        $nb_membres = Membre::count();
 
         $twig = new AdHocTwig();
 
@@ -60,25 +29,11 @@ final class Controller
         $twig->enqueueScript('/static/library/dataTables@2.3.6/dataTables.min.js');
         $twig->enqueueStyle('/static/library/dataTables@2.3.6/dataTables.min.css');
 
-        $twig->assign('membres', $membres);
-
-        $twig->assign('sort', $sort);
-        $twig->assign('sortinv', $sortinv);
-        $twig->assign('page', $page);
-
-        // test ajax
-        if (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && $_SERVER['HTTP_X_REQUESTED_WITH'] === 'XMLHttpRequest') {
-            return $twig->render('adm/membres/index-res.twig');
-        }
-
         $twig->assign('breadcrumb', [
             ['title' => '🏠', 'link' => '/'],
             ['title' => 'Privé', 'link' => '/adm'],
             'Membres',
         ]);
-
-        $twig->assign('types_membre', Membre::getTypesMembre());
-        $twig->assign('types_musicien', TypeMusicien::findAll());
 
         $twig->assign(
             'search',
@@ -89,11 +44,6 @@ final class Controller
                 'email' => $email,
             ]
         );
-
-        // pagination
-        $twig->assign('nb_items', $nb_membres);
-        $twig->assign('nb_items_per_page', self::MEMBERS_PER_PAGE);
-        $twig->assign('link_base_params', 'order_by=' . $order_by . '&sort=' . $sort);
 
         return $twig->render('adm/membres/index.twig');
     }
